1cc12f41544739e59c538154ff6472089ede7d30
zubairAbubakar <zuby52@gmail.com>
2020-12-24 00:45:14 +0100

    added media mentions section

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,27 @@ var typed = new Typed (".roles", {
     backSpeed: 60,
     loop: true
 });
+
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
